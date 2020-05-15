8e23728488bb3206117ad051e9206aa38d5c7217
Fedor Melekhin <fedormelexin@gmail.com>
2020-05-15 09:36:06 +0500

    Initial worked replacer

---
+++ inc/Main.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace WOOCICR\Inc;
+
+if ( ! class_exists( 'Main' ) ) {
+    class Main {
+        
+        /**
+         * Constructor
+         */
+        public function __construct() {
+            $this->rules = new Rules();
+
+            $this->rules->add_actions();
+
+            $this->setup_actions();
+            $this->menu_actions();
+        }
+
+        public function menu_actions() {
+            add_action( 'admin_menu', array( $this, 'menu_register' ) );
+            add_action( 'admin_head', function() {
+                RulesTable::admin_header();
+            } );
+        }
+
+         /**
+         * Menu register
+         */
+        public function menu_register () {
+            add_submenu_page(
+                'woocommerce',
+                'Woo CSV Import Category Replacer',
+                'WOOCICR',
+                'woocicr',
+                'woocicr',
+                array( $this, 'main_page' )
+            );
+        }
+        
+        /**
+         * Setting up Hooks
+         */
+        public function setup_actions() {
+            //Main plugin hooks
+            register_activation_hook( WOOCICR_MAIN_FILE, array( $this, 'activate' ) );
+            register_deactivation_hook( WOOCICR_MAIN_FILE, array( $this, 'deactivate' ) );
+        }
+        
+        /**
+         * Activate callback
+         */
+        public function activate() {
+            $this->rules->create_db();
+        }
+        
+        /**
+         * Deactivate callback
+         */
+        public function deactivate() {
+            $this->rules->remove_db();
+        }
+
+        /**
+         * Main page
+         */
+        public function main_page () {
+            $rules_table = new RulesTable();
+            $rules_table->prepare_items();
+            ?><div class="wrap">
+                <form action="" method="POST">
+                    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+                    <table class="rules-add-form">
+                        <tr>
+                            <th>From</th>
+                            <th>(or) Name</th>
+                            <th>To</th>
+                        </tr>
+                        <tr>
+                            <td><input name="from" value="" /></td>
+                            <td><input name="name" value="" /></td>
+                            <td><input name="to" value="" /></td>
+                        </tr>
+                    </table>
+                    <input name="action" value="add" type="hidden" />
+                    <?php submit_button( 'Add' ); ?>
+                </form>
+                <form action="" method="POST">
+                    <?php echo $rules_table->display(); ?>
+                </form>
+            </div><?php
+        }
+    }
+}
+?>
--- woo-csv-import-category-replacer.php
+++ woo-csv-import-category-replacer.php
@@ -1,5 +1,24 @@
 <?php
 /**
- * Plugin Name: Woo CSV Import Category Replacer
+ * Plugin Name:       Woo CSV Import Category Replacer
+ * Plugin URI:        https://wordpress.org/plugins/woo-csv-import-category-replacer/
+ * Description:       This plugin replaces substrings in a CSV Categories field to required by rules.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Fedor Melekhin <fedormelexin@gmail.com>
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       woocicr
  */
+namespace WOOCICR;
+
+
+define( 'WOOCICR_MAIN_FILE', __FILE__ );
+define( 'WOOCICR_DIR', plugin_dir_path( __FILE__ ) );
+
+require_once( WOOCICR_DIR . '/autoloader.php' );
+
+global $woocicr;
+$woocicr = new Inc\Main();
 ?>
